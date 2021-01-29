8f30adbac23c055b830040fd4e272be5a73ca4cf
Kommuri venkata varun <meetvarun.kommuri@gmail.com>
2021-01-29 20:21:30 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Modal } from "react-native";
+import Orientation from "react-native-orientation";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Modal style={{ flex: 1 }} visible={true}>
+        <View style={styles.container}>
+          <View
+            style={{
+              flex: 0.11,
+              backgroundColor: "blue",
+              alignItems: "center"
+            }}
+          >
+            <TouchableOpacity onPress={() => alert("hello")}>
+              <Text
+                style={{
+                  fontSize: 13,
+                  padding: 6,
+                  borderRadius: 5,
+                  backgroundColor: "white",
+                  color: "black",
+                  marginTop: 20,
+                  transform: [{ rotate: "-90deg" }]
+                }}
+              >
+                Done
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View
+            style={{
+              flex: 0.89,
+              alignItems: "center",
+              justifyContent: "center",
+              transform: [{ rotate: "-90deg" }]
+            }}
+          >
+            <Text>Image Box</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    //alignItems: 'center',
+    justifyContent: "center"
+  }
 });
